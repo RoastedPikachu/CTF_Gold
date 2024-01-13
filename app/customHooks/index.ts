72be03b20470b2ce0530afe8f14e0dabd61bb24f
Boris Karabut <boriskarabytwork@gmail.com>
2024-01-13 19:08:48 +0300

    Вынес реализацию таймера в кастомный хук для последующего переиспользования

---
+++ app/customHooks/index.ts
@@ -0,0 +1,13 @@
+export default function useCountdownInterval(setValueFunction: (value: string) => void, targetDate: number) {
+    return setInterval(() => {
+        const currentDate = new Date().getTime();
+
+        const dateDistance = targetDate - currentDate;
+
+        const daysLeft = Math.floor(dateDistance / (1000 * 60 * 60 * 24));
+        const hoursLeft = Math.floor((dateDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutesLeft = Math.floor((dateDistance % (1000 * 60 * 60)) / (1000 * 60));
+
+        setValueFunction(`${daysLeft < 10 ? `0${daysLeft}` : daysLeft}:${hoursLeft < 10 ? `0${hoursLeft}` : hoursLeft}:${minutesLeft < 10 ? `0${minutesLeft}` : minutesLeft}`);
+    }, 1000);
+}
