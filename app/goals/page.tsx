e4920680cbeee354bb8a13ebc2456d505768b0cc
Boris Karabut <boriskarabytwork@gmail.com>
2024-01-15 14:55:11 +0300

    Адаптировал таймер на странице целей под экраны мобильных телефонов
    Забыл это добавить в прошлый коммит

--- app/goals/page.tsx
+++ app/goals/page.tsx
@@ -15,11 +15,12 @@ export default function Page() {
       <TheHeader/>
 
       <main className="relative deskWide:mx-[calc((100%-1440px)/2)] w-full max-w-[1440px] h-[2310px] mlarge:h-auto overflow-y-hidden">
-        <CountdownInterval fontSize={"absolute mt-[620px] mlarge:mt-[445px] msmall:mt-[450px] w-full text-[7rem] mlarge:text-[3.5rem] mmedium:text-[3.125rem] msmall:text-[2.75rem] text-center"}/>
+        <CountdownInterval fontSize={"absolute mt-[620px] mlarge:mt-[450px] mmedium:mt-[455px] msmall:mt-[460px] w-full text-[7rem] mlarge:text-[2.375rem] mmedium:text-[2.125rem] msmall:text-[1.75rem] text-center"}/>
 
-          {!isMobile ? <img src="/static/MainPage/MainBackgroundImage.png" alt="" className="mt-[-60px] w-[1550px] h-[2380px] z-10"/> :
-
-        <img src="/static/MainPage/MainBackgroundMobileImage.png" alt="" className="mt-[-60px] w-full h-[1800px] z-10"/>}
+        {!isMobile ?
+            <img src="/static/MainPage/MainBackgroundImage.png" alt="" className="mt-[-60px] w-[1550px] h-[2380px] z-10"/>
+        :
+            <img src="/static/MainPage/MainBackgroundMobileImage.png" alt="" className="mt-[-60px] w-full h-[1800px] z-10"/>}
       </main>
 
       <TheFooter position={"absolute mt-[-310px]"}/>
