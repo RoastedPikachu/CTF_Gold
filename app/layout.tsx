690caad8849da1c60555fedd57bab58e6faceb17
Boris Karabut <boriskarabytwork@gmail.com>
2024-01-15 14:43:10 +0300

    Внедрил яндекс метрику

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from 'next'
+
 import { Inter } from 'next/font/google'
+
+import Script from 'next/script';
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,7 +21,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Script src="/static/scripts/metrica.js" strategy="lazyOnload" />
+
+        {children}
+      </body>
     </html>
   )
 }
